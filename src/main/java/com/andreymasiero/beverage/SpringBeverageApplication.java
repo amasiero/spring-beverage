4d8b08a924d24b0733e17ac6be666d4af193bfcc
Andrey Masiero <andreymasiero@gmail.com>
2023-02-20 14:50:39 +0000

    feat: Initial project
    create the initial structure of the project to understand lombok
    project.

---
+++ src/main/java/com/andreymasiero/beverage/SpringBeverageApplication.java
@@ -0,0 +1,13 @@
+package com.andreymasiero.beverage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeverageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeverageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andreymasiero/beverage/SpringBeverageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andreymasiero.beverage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeverageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
