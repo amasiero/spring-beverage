fbf1b72e21ea97d59562765dc74927b065294cde
Andrey Masiero <andreymasiero@gmail.com>
2023-02-21 09:43:03 +0000

    feat: Create a POJO class with Lombok
    Lombok can simplify all the code writed. We use the @Data annotation
    which combines @Getter @Setter @RequiredArgsConstructor
    @ToString @EqualsAndHashCode.
    
    When the project is compiled then Lombok creates all the setters,
         getters, constructor with required arguments, and the base methods
         like toString, equals, and hashCode.

---
+++ src/main/java/com/andreymasiero/beverage/model/Beer.java
@@ -0,0 +1,19 @@
+package com.andreymasiero.beverage.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Data;
+
+@Data
+public class Beer {
+    private UUID id;
+    private Integer version;
+    private String name;
+    private BeerStyle style;
+    private String upc;
+    private Integer quantityOnHand;
+    private BigDecimal price;
+    private LocalDateTime createdOn;
+    private LocalDateTime updatedOn;
+}
---
+++ src/main/java/com/andreymasiero/beverage/model/BeerStyle.java
@@ -0,0 +1,14 @@
+package com.andreymasiero.beverage.model;
+
+public enum BeerStyle {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
